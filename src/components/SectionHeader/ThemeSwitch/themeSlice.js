894d8e454f6f6dbd266066d536bdcb9f2c2d9790
Kinga Tos <toskinga@interia.pl>
2023-04-19 20:31:01 +0200

    added themeSlice

---
+++ src/components/SectionHeader/ThemeSwitch/themeSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  getThemeFromLocalStorage,
+  saveThemeInLocalStorage,
+} from "./themeLocalStorage";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    isDarkTheme: getThemeFromLocalStorage(),
+  },
+  reducers: {
+    toggleTheme: (state) => {
+      state.isDarkTheme = !state.isDarkTheme;
+      saveThemeInLocalStorage(state.isDarkTheme);
+    },
+  },
+});
+export const { toggleTheme } = themeSlice.actions;
+export const selectThemeState = state => state.theme;
+export const selectIsDarkTheme = (state) => selectThemeState(state).isDarkTheme;
+
+export default themeSlice.reducer;
