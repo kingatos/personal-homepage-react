ba8272072393f347138ddc03915822f2395669f0
Kinga Tos <toskinga@interia.pl>
2023-04-21 20:25:13 +0200

    created SkillsInner section

---
+++ src/components/SectionSkills/SkillsInner/index.js
@@ -0,0 +1,31 @@
+import {
+  WrapperSkills,
+  Subheader,
+  SkillsList,
+  Icon,
+  ListItem,
+  Point,
+  Divider,
+} from "./styled.js";
+
+function SkillsInner({ title, icon, list }) {
+  return (
+    <WrapperSkills>
+      <Subheader>
+        {title}
+        <Icon>{icon}</Icon>
+      </Subheader>
+      <Divider />
+      <SkillsList>
+        {list.map((item) => (
+          <ListItem key={item}>
+            <Point />
+            {item}
+          </ListItem>
+        ))}
+      </SkillsList>
+    </WrapperSkills>
+  );
+};
+
+export default SkillsInner;
