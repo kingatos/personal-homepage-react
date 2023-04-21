65fbabfebf14623337ca24dcee543bfd726b9c77
Kinga Tos <toskinga@interia.pl>
2023-04-21 20:25:52 +0200

    created ThemeSwitch component

---
+++ src/components/SectionHeader/ThemeSwitch/index.js
@@ -0,0 +1,33 @@
+import {
+  Wrapper,
+  BackgroundText,
+  ThemeSwitchButton,
+  SwitchElement,
+} from "./styled";
+import light from "./icon/light.svg";
+import dark from "./icon/dark.svg";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkTheme, toggleTheme } from "./themeSlice";
+
+function ThemeSwitch() {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      <BackgroundText>Dark mode {isDarkTheme ? "on" : "off"} </BackgroundText>
+      <ThemeSwitchButton
+        onClick={() => dispatch(toggleTheme())}
+        title="change a theme"
+      >
+        <SwitchElement
+          src={isDarkTheme ? dark : light}
+          alt="Change Theme"
+          moveRight={isDarkTheme}
+        />
+      </ThemeSwitchButton>
+    </Wrapper>
+  );
+}
+
+export default ThemeSwitch;
