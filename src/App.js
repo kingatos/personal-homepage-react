88f9d4085a61076861f3159aed2aded6a3e9f65e
Kinga Tos <toskinga@interia.pl>
2023-04-21 20:24:41 +0200

    modificated code, added provider, useSelector, GlobalStyles and anothers

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import SectionHeader from "./components/SectionHeader/index.js";
-import { StyledContainer } from "./components/Container/styled.js";
+import SectionSkills from "./components/SectionSkills/index.js";
+import { AppContainer, AppWrapper } from "./components/Container/styled.js";
+import { GlobalStyles } from "./GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "./components/SectionHeader/ThemeSwitch/themeSlice";
+import { darkTheme, lightTheme } from "./theme.js";
+
 
 function App() {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
   return (
-    <StyledContainer>
-     <SectionHeader />
-    </StyledContainer>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <>
+        <GlobalStyles />
+        <AppWrapper>
+        <AppContainer>
+          <SectionHeader />
+          <SectionSkills />
+        </AppContainer>
+        </AppWrapper>
+      </>
+    </ThemeProvider>
   );
 }
 
