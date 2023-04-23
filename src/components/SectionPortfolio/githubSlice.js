39adc19ee9a1692af15477dee7c17a020b46211f
Kinga Tos <toskinga@interia.pl>
2023-04-23 21:29:57 +0200

    created githubSlice

---
+++ src/components/SectionPortfolio/githubSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const githubSlice = createSlice({
+  name: "github",
+  initialState: {
+    reposStatus: "loading",
+    repos: [],
+  },
+  reducers: {
+    fetchGithubRepos: (state) => {
+      state.reposStatus = "loading";
+    },
+
+    fetchGithubReposSuccess: (state, { payload: repos }) => {
+      state.reposStatus = "success";
+      state.repos = repos;
+    },
+
+    fetchGithubReposError: (state) => {
+      state.reposStatus = "error";
+    },
+  },
+});
+
+export const {
+  fetchGithubRepos,
+  fetchGithubReposSuccess,
+  fetchGithubReposError,
+} = githubSlice.actions;
+export const selectGithubState = (state) => state.github;
+export const selectRepos = (state) => selectGithubState(state).repos;
+export const selectReposStatus = state => selectGithubState(state).reposStatus;
+
+export default githubSlice.reducer;
