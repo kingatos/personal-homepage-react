617fe4231b7127c5beffe1aef9ad2d25b8d0792e
Kinga Tos <toskinga@interia.pl>
2023-04-24 20:34:13 +0200

    created styled Portfolio section

---
+++ src/components/SectionPortfolio/styled.js
@@ -0,0 +1,70 @@
+import styled, { css } from "styled-components";
+import { Subheader } from "../SectionSkills/SkillsInner/styled";
+import { githubIcon } from "./github.svg";
+
+export const WrapperPortfolio = styled.section`
+  text-align: center;
+  margin: 72px 0;
+  word-break: break-word;
+
+  text-align: center;
+  margin: 72px 0;
+  word-break: break-word;
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    margin: 60px 0;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    margin: 48px 0;
+  }
+`;
+
+export const LinkPortfolio = styled.a`
+  text-decoration: none;
+`;
+
+export const HeaderPortfolio = styled(Subheader)`
+  text-align: center;
+  margin: 0 0 8px 0;
+  padding-top: 8px;
+  display: grid;
+`;
+
+export const GitHubIcon = styled(githubIcon)`
+  width: 40px;
+  height: 40px;
+  transition: 0.5s;
+
+  fill: ${({ theme }) => theme.elementColor.portfolio.icon};
+  &:hover {
+    filter: brightness(120%);
+  }
+`;
+
+export const SubHeaderPortfolio = styled.p`
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 140%;
+  letter-spacing: 0.05em;
+  color: ${({ theme }) => theme.elementColor.portfolio.lead};
+  transition: 1s;
+  margin: 0;
+  padding-bottom: 24px;
+`;
+
+export const WrapperRepos = styled.div`
+  margin: 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 32px;
+  transition: 1s;
+
+  ${({ notSuccess }) =>
+    notSuccess &&
+    css`
+      grid-template-columns: auto;
+    `}
+    
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    grid-template-columns: 1fr;
+  } ;
+`;
