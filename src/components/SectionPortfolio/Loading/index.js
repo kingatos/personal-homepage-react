65b21de9d8620705752fd2d8b85497435ec768ba
Kinga Tos <toskinga@interia.pl>
2023-04-26 16:53:53 +0200

    created Loading component

---
+++ src/components/SectionPortfolio/Loading/index.js
@@ -0,0 +1,13 @@
+import spinner from "./spinner.svg";
+import { Wrapper, Info, Spinner } from "./styled";
+
+const Loading = () => (
+  <>
+    <Wrapper>
+      <Info>Please wait, projects are begin loaded...</Info>
+      <Spinner src={spinner} alt="spinner"></Spinner>
+    </Wrapper>
+  </>
+);
+
+export default Loading;
