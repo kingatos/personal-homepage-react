b2460caa9b636afaed4ef029dab42b3c09df51c8
Kinga Tos <toskinga@interia.pl>
2023-04-30 22:24:03 +0200

    created rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,5 +1,6 @@
 import { all } from "redux-saga/effects";
+import { githubSaga } from "./components/SectionPortfolio/githubSaga";
 
 export default function* rootSaga() {
-    yield all([]);
+    yield all([githubSaga()]);
 }
