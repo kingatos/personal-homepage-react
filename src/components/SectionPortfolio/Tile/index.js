2daf4e326b2ce7aa4ab6e829a551e6adf4194c5b
Kinga Tos <toskinga@interia.pl>
2023-05-03 13:26:06 +0200

    created in SectionPortfolio function GithubTile with styled.js

---
+++ src/components/SectionPortfolio/Tile/index.js
@@ -0,0 +1,40 @@
+import {
+  WrapperTile,
+  Title,
+  Descriprion,
+  LinksWrapper,
+  LinkWrapper,
+  Link,
+} from "./styled";
+
+function GitHubTile({ name, description, gitUrl, demo }) {
+  const formattedName = (name[0].toUpperCase() + name.substring(1)).replaceAll("-", " ");
+
+  return (
+    <WrapperTile title={`repository name: ${name}`}>
+      <Title>{formattedName}</Title>
+      <Descriprion>{description && description}</Descriprion>
+      {demo && <LinksWrapper>
+          <dt>Demo:</dt>
+          <LinkWrapper>
+            <Link href={demo} target="_blank" rel="noreferrer">
+              {demo}
+            </Link>
+          </LinkWrapper>
+        </LinksWrapper>
+      }
+      {gitUrl && 
+        <LinksWrapper>
+          <dt>Repo:</dt>
+          <LinkWrapper>
+            <Link href={gitUrl} target="_blank" rel="noreferrer">
+              {gitUrl}
+            </Link>
+          </LinkWrapper>
+        </LinksWrapper>
+      }
+    </WrapperTile>
+  );
+};
+
+export default GitHubTile;
---
+++ src/components/SectionPortfolio/Tile/styled.js
@@ -0,0 +1,127 @@
+import styled from "styled-components";
+import { borderRadius, boxShadow } from "../../../theme";
+
+export const WrapperTile = styled.div`
+  background: ${({ theme }) => theme.elementColor.portfolio.background};
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  box-shadow: ${boxShadow};
+  border-radius: ${borderRadius};
+  padding: 56px;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.4;
+  letter-spacing: 0.05em;
+  margin: 0 0 24px 0;
+  text-align: left;
+  transition: 0.5s;
+  :hover {
+    border: 6px solid ${({ theme }) => theme.elementColor.portfolio.tileHover};
+    box-shadow: ${boxShadow};
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    padding: 36px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    padding: 28px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    padding: 16px;
+  }
+`;
+
+export const Title = styled.h3`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 1.4;
+  letter-spacing: 0.05em;
+  color: ${({ theme }) => theme.elementColor.portfolio.title};
+  margin: 0 0 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 16px;
+    line-height: 1.2;
+    margin-bottom: 16px;
+  }
+`;
+
+export const Descriprion = styled.p`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.4;
+  color: ${({ theme }) => theme.elementColor.portfolio.description};
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    font-size: 17px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 14px;
+    line-height: 1.2;
+    margin: 0 0 8px 0;
+  }
+`;
+
+export const LinksWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 4em 1fr;
+  margin: 24px 0 0 0;
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.4;
+  color: ${({ theme }) => theme.elementColor.portfolio.description};
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    font-size: 17px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 14px;
+    line-height: 1.2;
+    margin: 16px 0 0 0;
+     }
+  `;
+
+export const Demo = styled(Descriprion)`
+  margin: 8px 0 0 0;
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 14px;
+    line-height: 1.2;
+  }
+`;
+
+export const Repo = styled(Descriprion)`
+  margin: 8px 0 0 0;
+`;
+
+export const LinkWrapper = styled.dd`
+  margin: 0;
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    font-size: 17px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 14px;
+    line-height: 1.2;
+  }
+ `;
+
+export const Link = styled.a`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 1.4;
+  color: ${({ theme }) => theme.elementColor.portfolio.link};
+  transition: 0.3s;
+  text-decoration: none;
+  word-break: break-word;
+  opacity: 0.8;
+  border-bottom: 1px solid ${({ theme }) => theme.elementColor.portfolio.underline};
+ 
+  &:hover {
+    opacity: 1;
+  }
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigScreen}px) {
+    font-size: 17px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoint.bigMobile}px) {
+    font-size: 14px;
+    line-height: 1.2;
+  }
+`;
