f216349ae3309dc8a6e9cc5b02fad9fb9f6acfaf
Kinga Tos <toskinga@interia.pl>
2023-05-16 19:56:19 +0200

    removed items to core folder

--- src/rootSaga.js
+++
@@ -1,6 +0,0 @@
-import { all } from "redux-saga/effects";
-import { githubSaga } from "./components/SectionPortfolio/githubSaga";
-
-export default function* rootSaga() {
-  yield all([githubSaga()]);
-}
